a34429b2bc0dc39c68eb5fec2d62bc91420bdb7c
nv1ruz <braian.martz@gmail.com>
2020-03-23 13:17:08 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, } from '@angular/core';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-mqtt';
+  // private subscription: Subscription;
+  // topicname: any;
+  // msg: any;
+  // isConnected: boolean = false;
+  // @ViewChild('msglog', { static: true }) msglog: ElementRef;
+
+  public subsTopic: Subscription;
+  public topicName: any;
+  public message: any;
+
+  constructor( private _mqttService: MqttService ) {}
+
+  ngOnDestroy(): void {
+    // this.subscription.unsubscribe();
+  }
+
+
+  public suscribirseAtopic(): void {
+    console.warn('Suscripto a topic: ', this.topicName);
+    this.subsTopic = this._mqttService.observe( this.topicName ).subscribe( (message:IMqttMessage) => {
+      console.log('Mensaje obtenido: ', message);
+      console.log('Payload obtenido: ', message.payload.toString());
+    } );
+  }
+
+  public cancelarSuscripcion(): void {
+    if( this.subsTopic ) {
+      this.subsTopic.unsubscribe();
+      this.subsTopic = null;
+      console.error('Suscripción cancelada');
+    }
+  }
+
+  public enviarMensaje(): void {
+    this._mqttService.unsafePublish( this.topicName, this.message, { qos: 1, retain:true });
+    console.warn('Mensaje enviado: ', this.message);
+  }
+
+  // subscribeNewTopic(): void {
+  //   console.log('inside subscribe new topic')
+  //   this.subscription = this._mqttService.observe(this.topicname).subscribe((message: IMqttMessage) => {
+  //     this.msg = message;
+  //     console.log('msg: ', message)
+  //     console.log('msg: ', message.payload.toString())
+  //     this.logMsg('Message: ' + message.payload.toString() + '<br> for topic: ' + message.topic);
+  //   });
+  //   this.logMsg('subscribed to topic: ' + this.topicname)
+  // }
+
+  // sendmsg(): void {
+  //   // use unsafe publish for non-ssl websockets
+  //   this._mqttService.unsafePublish(this.topicname, this.msg, { qos: 1, retain: true })
+  //   this.msg = ''
+  // }
+  
+  // logMsg(message): void {
+  //   this.msglog.nativeElement.innerHTML += '<br><hr>' + message;
+  // }
+
+  // clear(): void {
+  //   this.msglog.nativeElement.innerHTML = '';
+  // }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { MqttModule, IMqttServiceOptions } from "ngx-mqtt";
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: '190.7.57.163',
+  port: 8093,
+  path: '/mqtt'
+}
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +15,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
